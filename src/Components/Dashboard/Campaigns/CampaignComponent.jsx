07202ca32e754d085435b09a680e5d0c83440fd1
Nikkhil420 <nikhilbujade419@gmail.com>
2024-07-08 11:59:23 +0530

    Navbar+Campaign Changes

--- src/Components/Dashboard/Campaigns/CampaignComponent.jsx
+++ src/Components/Dashboard/Campaigns/CampaignComponent.jsx
@@ -15,29 +15,41 @@ const CampaignComponent = () => {
 
   return (
     <div className=" max-h-[296px]" >
-     <div class="flex flex-col bg-green-100 p-4 min-h-[138px] min-w-72  rounded-lg  w-1/3">
-      <div class="flex justify-between  items-center">
-        <div>
-          <p class="text-sm text-green-700">ACTIVE CAMPAIGNS</p>
-          <p class="text-3xl font-bold">{activeCampaignsCount}</p>
-        </div>
-        <button class="bg-green-200 p-2 rounded-full">
-          <KeyboardArrowRightIcon/>
-        </button>
-      </div>
-    </div>
+    
+    <div class="flex flex-col justify-between bg-green-100 p-4 min-h-[138px] min-w-72 rounded-lg">
 
-    <div class="flex flex-col bg-red-100 p-4 min-h-[138px] min-w-72 mt-8 rounded-lg w-1/3">
-      <div class="flex justify-between items-center">
-        <div>
-          <p class="text-sm text-red-700">PAST CAMPAIGNS</p>
-          <p class="text-3xl font-bold">{pastCampaignsCount}</p>
-        </div>
-        <button class="bg-red-200 p-2 rounded-full">
-          <KeyboardArrowRightIcon />
-        </button>
-      </div>
+  <div class="flex justify-between items-center ">
+    <div>
+      <p class="text-base text-[#57595A]-700">ACTIVE CAMPAIGNS</p>
+      <p class="text-3xl font-bold">{activeCampaignsCount}</p>
     </div>
+  </div>
+
+
+  <div class=" flex justify-end">
+    <button class="bg-white p-2 mr-4 rounded-full">
+      <KeyboardArrowRightIcon />
+    </button>
+  </div>
+</div>
+
+
+    <div class="flex flex-col justify-between bg-red-100 p-4 min-h-[138px] min-w-72 mt-4 rounded-lg">
+
+  <div class="flex justify-between items-center mb-4">
+    <div>
+      <p class="text-base  text-[#57595A]-700">PAST CAMPAIGNS</p>
+      <p class="text-3xl font-bold">{pastCampaignsCount}</p>
+    </div>
+  </div>
+  
+
+  <div class="flex justify-end">
+    <button class="bg-white p-2 mr-4 rounded-full">
+      <KeyboardArrowRightIcon />
+    </button>
+  </div>
+</div>
     </div>
   );
 };
