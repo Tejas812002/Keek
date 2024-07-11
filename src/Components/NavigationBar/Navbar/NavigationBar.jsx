ae00db54fc97db2013470ffdd4b8d1b487cff373
Siddhesh Ashtikar <siddheshashtikar00@outlook.com>
2024-07-11 11:05:54 +0530

    Navigation icons

--- src/Components/NavigationBar/Navbar/NavigationBar.jsx
+++ src/Components/NavigationBar/Navbar/NavigationBar.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import SearchIcon from '../../../Assets/SearchIcon.png';
-import Bellicon from '../../../Assets/bell.png';
-import messageIcon from '../../../Assets/message-outline.png';
-import trophyIcon from '../../../Assets/trophy.png';
+import { CiSearch } from "react-icons/ci";
+import { CgBell, CgTrophy } from "react-icons/cg";
+import { FiMessageSquare } from "react-icons/fi";
 
 
 const NavigationBar = ({ expanded }) => {
@@ -10,20 +9,23 @@ const NavigationBar = ({ expanded }) => {
 
     <div   className={`fixed z-10 top-0 ${!expanded && "left-[78px] w-[calc(100%-65px)]"} left-[311px] w-[calc(100%-311px)] bg-white h-24 flex items-center justify-between bg-background  px-4`}>
   
-    <div className="relative flex items-center left-8 w-8/12"> 
-      <img src={SearchIcon} alt="magnifier" className="absolute left-8  top-1/2 transform -translate-y-1/2" />
-      <input
-        type="text"
-        placeholder="Search"
-        className="w-full h-11 bg-[#F5F5F5]  pl-16 py-1 rounded bg-input text-foreground border border-border focus:outline-none focus:ring-2 focus:ring-ring"
-      />
-    </div>
+  <div className="relative flex items-center w-8/12">
+  <div className="absolute pl-3">
+    <CiSearch className='size-6' />
+  </div>
+  <input
+    type="text"
+    placeholder="Search"
+    className="w-full h-11 bg-[#F5F5F5] pl-10 pr-4 py-1 rounded bg-input text-foreground border border-border focus:outline-none focus:ring-2 focus:ring-ring"
+  />
+</div>
+
  
   <div className="flex items-center  w-44 h-8 pr-4 justify-between space-x-2"> 
     {/* Add onclick for adding functions */}
-    <img src={messageIcon} alt="messages" className="w-6 h-6" />
-    <img src={trophyIcon} alt="bell" className="w-8 h-8" />
-    <img src={Bellicon} alt="notification" className="w-8 h-8" />
+    <FiMessageSquare className='size-7'/>
+    <CgTrophy className='size-9'/>
+    <CgBell className='size-8'/>
   </div>
 </div>
 
